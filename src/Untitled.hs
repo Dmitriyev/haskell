98ebfe7c3ea9d6308117ec3870a2223e9f21bd1c
fpuser1719655 <evseev.dmsr@gmail.com>
2015-10-14 13:51:26 -0700

    first lab

--- src/Untitled.hs
+++ src/Untitled.hs
@@ -1,10 +1,13 @@
 module Untitled where
-    
+
 fact i = if i <= 1 then 1 else i * fact (i - 1)
+strfact sx =
+    let param = read sx in
+    show (fact param)
 
 -- | The main entry point.
 main :: IO ()
 main = do
     putStrLn "enter the number, which will assume the factorial"
     i <- getLine 
-    putStrLn  i 
+    putStrLn (strfact i)
