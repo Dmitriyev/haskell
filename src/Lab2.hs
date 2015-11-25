5cb95ac473347abba50995e67d4a0bf38d5a2cbf
fpuser1719655 <evseev.dmsr@gmail.com>
2015-11-25 12:03:53 -0800

    2 lab

--- src/Lab2.hs
+++ src/Lab2.hs
@@ -9,14 +9,19 @@ emptyTree = Empty
 -- Добавления элемента 
 insert :: BinaryTree -> Integer -> BinaryTree
 insert (Empty) a = Node Empty a Empty
-insert (Node left x right) a 
-    | a > x = Node left x insert right a
-    | a < x = Node insert left a x right
+insert (Node left x right) a
+    | a > x = Node left x (insert right a)
+    | a < x = Node (insert left a) x right
     | a == x = Node left x right
 
 --Удаление элемента
 remove :: BinaryTree -> Integer -> BinaryTree
-remove (Empty) :: putStrLn "error: empty tree"
+remove (Empty) a = Empty
 remove (Node left x right) a
-    | a > x = Node left x remove right a
-    | a < x = Node remove left a x right
+    | a > x = Node left x (remove right a)
+    | a < x = Node (remove left a) x right
+    | a == x =
+        let if isEmpty right
+        then left
+        else Node 
+        
